7a50086689009d32ad5532bac7a14236b0150a64
pedrini.edoardo <pedrini.edoardo@hsr.it>
2025-09-30 10:53:12 +0200

    comment the scripts not used yet

--- analysis/R_analysis/scr/06_Enrichr.R
+++ analysis/R_analysis/scr/06_Enrichr.R
@@ -1,53 +1,53 @@
-# AIM ---------------------------------------------------------------------
-# run EnrichR with a panel of annotations
-
-# libraries ---------------------------------------------------------------
-library(enrichR)
-library(tidyverse)
-
-# DB selection ------------------------------------------------------------
-dbs <- listEnrichrDbs()
-#filter fo the db of interest
-dbs %>%
-  dplyr::filter(str_detect(libraryName,pattern = "Atlas"))
-
-dbs %>%
-  dplyr::filter(str_detect(libraryName,pattern = "KEGG"))
-
-dbs_db <- c("KEGG_2021_Human","MSigDB_Hallmark_2020","Reactome_2022","Human_Gene_Atlas","Azimuth_Cell_Types_2021")
-
-# GENE SELECTION ----------------------------------------------------------
-# save the ranked object, also change the genes into genenames
-full_df <- read_tsv("../../out/table/res_BMP9_vs_Mock_shr.txt")
-
-list_res_tot <- list(BMP9_vs_mock = full_df %>%
-                       dplyr::filter(padj<0.05,abs(log2FoldChange)>1) %>%
-                       pull(symbol))
-
-# query -------------------------------------------------------------------
-list <- lapply(list_res_tot,function(x){
-  genes <- x
-  out_enrich <- enrichr(genes, dbs_db)
-  #
-  out_enrich
-})
-
-df_enrichr_annotation_enriched_tot <- list$BMP9_vs_mock %>%
-  bind_rows(.id = "annotation")
-
-df_enrichr_annotation_enriched_tot %>%
-  write_tsv("../../out/table/enrichR_out_BMP9_vs_mock_shr.tsv")
-
-library(scales)
-df_enrichr_annotation_enriched_tot %>%
-  group_by(annotation) %>%
-  arrange(P.value) %>%
-  dplyr::slice(1:20) %>%
-  mutate(Term = str_sub(Term,start = 1,end = 30)) %>%
-  mutate(Term = fct_reorder(Term, Combined.Score,.desc = F)) %>%
-  ggplot(aes(y=Term,x=Combined.Score,size = Odds.Ratio,col = Adjusted.P.value)) + geom_point() + facet_wrap(~annotation,ncol = 1,scales = "free")+theme_bw() +
-  scale_color_gradientn(colors = c("red","blue"),
-                        values = rescale(c(0,1)),
-                        limits = c(0,0.2))+theme(strip.background = element_blank())
-# scale_color_gradient(low = "red",high = "blue")
-ggsave("../../out/plot/enrichR_out_BMP9_vs_mock_shr_plot.pdf",width = 7,height = 15)
+# # AIM ---------------------------------------------------------------------
+# # run EnrichR with a panel of annotations
+# 
+# # libraries ---------------------------------------------------------------
+# library(enrichR)
+# library(tidyverse)
+# 
+# # DB selection ------------------------------------------------------------
+# dbs <- listEnrichrDbs()
+# #filter fo the db of interest
+# dbs %>%
+#   dplyr::filter(str_detect(libraryName,pattern = "Atlas"))
+# 
+# dbs %>%
+#   dplyr::filter(str_detect(libraryName,pattern = "KEGG"))
+# 
+# dbs_db <- c("KEGG_2021_Human","MSigDB_Hallmark_2020","Reactome_2022","Human_Gene_Atlas","Azimuth_Cell_Types_2021")
+# 
+# # GENE SELECTION ----------------------------------------------------------
+# # save the ranked object, also change the genes into genenames
+# full_df <- read_tsv("../../out/table/res_BMP9_vs_Mock_shr.txt")
+# 
+# list_res_tot <- list(BMP9_vs_mock = full_df %>%
+#                        dplyr::filter(padj<0.05,abs(log2FoldChange)>1) %>%
+#                        pull(symbol))
+# 
+# # query -------------------------------------------------------------------
+# list <- lapply(list_res_tot,function(x){
+#   genes <- x
+#   out_enrich <- enrichr(genes, dbs_db)
+#   #
+#   out_enrich
+# })
+# 
+# df_enrichr_annotation_enriched_tot <- list$BMP9_vs_mock %>%
+#   bind_rows(.id = "annotation")
+# 
+# df_enrichr_annotation_enriched_tot %>%
+#   write_tsv("../../out/table/enrichR_out_BMP9_vs_mock_shr.tsv")
+# 
+# library(scales)
+# df_enrichr_annotation_enriched_tot %>%
+#   group_by(annotation) %>%
+#   arrange(P.value) %>%
+#   dplyr::slice(1:20) %>%
+#   mutate(Term = str_sub(Term,start = 1,end = 30)) %>%
+#   mutate(Term = fct_reorder(Term, Combined.Score,.desc = F)) %>%
+#   ggplot(aes(y=Term,x=Combined.Score,size = Odds.Ratio,col = Adjusted.P.value)) + geom_point() + facet_wrap(~annotation,ncol = 1,scales = "free")+theme_bw() +
+#   scale_color_gradientn(colors = c("red","blue"),
+#                         values = rescale(c(0,1)),
+#                         limits = c(0,0.2))+theme(strip.background = element_blank())
+# # scale_color_gradient(low = "red",high = "blue")
+# ggsave("../../out/plot/enrichR_out_BMP9_vs_mock_shr_plot.pdf",width = 7,height = 15)
--- analysis/R_analysis/scr/07_plot_some_genes_01.R
+++ analysis/R_analysis/scr/07_plot_some_genes_01.R
@@ -1,109 +1,109 @@
-# AIM ---------------------------------------------------------------------
-# plot some genes
-
-# libraries ---------------------------------------------------------------
-library(tidyverse)
-library(DESeq2)
-library(ComplexHeatmap)
-
-# plot genes with heatmap -------------------------------------------------
-vds_filter <- readRDS(file = "../../out/object/vds_all_filter.rds")
-
-# gene_id <- rownames(assay(vds_filter)) %in% c("XIST","DDX3Y","RPS4Y1","USP9Y")
-# gene_id <- rownames(assay(vds_filter)) %in% c("VCAN","CXCL12","ID2","ABI3BP","APLP1","MGP","TIMP3","SPOCK1","THY1","NID2","FBLN5","VEGFA")
-# gene_id <- rownames(assay(vds_filter)) %in% c("SELP","C7","CPE")
-gene_id <- rownames(assay(vds_filter)) %in% c("GJA5","SEMA3G","DKK2","FBLN5","CXCL12","ID2","ABI3BP","APLP1","MGP","SPOCK1","FBLN5","VEGFA","SELP","C7","CPE")
-
-# notice that for both dataset the design didn't affect mucht he normalizationo,therefore the topmost variable genes are the same for both
-# I will prduce the plot for just one dataset
-# The heatmap becomes more interesting if we do not look at absolute expression strength but rather at the amount by which each gene deviates in a specific sample from the gene’s average across all samples. Hence, we center each genes’ values across samples, and plot a heatmap (figure below). We provide a data.frame that instructs the pheatmap function how to label the columns.
-# mat <- assay(vds_filter)[topVarGenes, ]
-mat <- assay(vds_filter)[gene_id, ]
-mat2 <- (mat - rowMeans(mat))/rowSds(mat)
-
-# change the rownames to match the symbol 
-# rownames(mat2) <- rownames(mat2) %>% 
-#   data.frame(ensembl=.) %>% 
-#   left_join(DEG_1,by = "ensembl") %>% 
-#   pull(symbol) 
-
-# build the annotation object  
-#
-sample_ordered <- data.frame(sample = colnames(mat2)) %>%
-  left_join(vds_filter@colData %>%
-              data.frame(),by="sample")
-
-# update the column name of the matrix
-colnames(mat2) <- sample_ordered$sample_name
-
-column_ha <- HeatmapAnnotation(treat = sample_ordered$treat,
-                               gender = sample_ordered$gender,
-                               col = list(treat = c("mock" = "gray", "BMP9" = "black"),
-                                          gender = c("M" = "blue", "F" = "pink")))
-
-ht2 <- Heatmap(mat2, 
-               name = "exp",
-               top_annotation = column_ha, 
-               # cluster_rows = F, 
-               # col = colorRamp2(c(-2, 0, 1), c("green", "white", "red")),
-               # right_annotation = row_ha, 
-               # row_split = rep(c(1,2,3,4),c(2,3,4,7)),
-               column_title = "GOI") 
-
-pdf("../../out/plot/heatmap_GOI_01.pdf",width = 6,height = 6) 
-draw(ht2,heatmap_legend_side = "left",annotation_legend_side = "left") 
-dev.off()
-
-# plot genes dotplot ------------------------------------------------------
-data <- readRDS("../../out/object/dds_all_filter_DESeq.rds")
-
-lut <- data@colData %>%
-  data.frame()
-
-# GOI <- c("SMAD1","SMAD9","SMAD6","BMPR1B","SMAD7")
-# GOI <- subset_genes
-# GOI <- c("VCAN","CXCL12","ID2","ABI3BP","APLP1","MGP","TIMP3","SPOCK1","THY1","NID2","FBLN5","VEGFA")
-# GOI <- c("SELP","C7","CPE")
-# GOI <- c("GJA5","SEMA3G","DKK2","FBLN5")
-GOI <- c("GJA5","SEMA3G","DKK2","FBLN5","CXCL12","ID2","ABI3BP","APLP1","MGP","SPOCK1","FBLN5","VEGFA","SELP","C7","CPE")
-
-MR <- counts(data,normalized=T)%>%
-  data.frame()%>%
-  rownames_to_column("symbol") %>%
-  pivot_longer(names_to = "sample",values_to = "exp",-symbol)%>%
-  group_by(sample)%>%
-  summarise(MR = sum(exp)/10^6)
-
-# plot the data following the methods implemented in the plotCounts funciton from DESeq2
-# Normalized counts plus a pseudocount of 0.5 are shown by default.
-counts(data,normalized=T)%>%
-  data.frame()%>%
-  rownames_to_column("symbol") %>%
-  dplyr::filter(symbol %in% GOI) %>%
-  pivot_longer(names_to = "sample",values_to = "count",-symbol) %>%
-  # add the milion reads per sample
-  left_join(MR,by = "sample") %>%
-  left_join(lut,by = "sample") %>%
-  mutate(count_norm_adj = count + 0.5)%>%
-  ggplot(aes(x=treat,y = count_norm_adj))+
-  geom_boxplot(outlier.shape = NA)+
-  geom_point(position = position_jitter(width = 0.1),alpha=0.6)+facet_wrap(~symbol,scales = "free")+scale_y_log10()+ theme_bw()+
-  theme(strip.background = element_blank(), 
-        panel.border = element_rect(colour = "black", fill = NA))
-ggsave("../../out/plot/boxplot_GOI_01.pdf",width = 8,height = 8) 
-
-counts(data,normalized=T)%>%
-  data.frame()%>%
-  rownames_to_column("symbol") %>%
-  dplyr::filter(symbol %in% GOI) %>%
-  pivot_longer(names_to = "sample",values_to = "count",-symbol) %>%
-  # add the milion reads per sample
-  left_join(MR,by = "sample") %>%
-  left_join(lut,by = "sample") %>%
-  mutate(count_norm_adj = count + 0.5)%>%
-  ggplot(aes(x=treat,y = count_norm_adj,group=clone,col=clone))+geom_point(alpha=0.6)+
-  geom_line() +
-  facet_wrap(~symbol,scales = "free")+scale_y_log10()+ theme_bw()+
-  theme(strip.background = element_blank(), 
-        panel.border = element_rect(colour = "black", fill = NA))
-ggsave("../../out/plot/scatterplot_GOI_01.pdf",width = 8,height = 8)
+# # AIM ---------------------------------------------------------------------
+# # plot some genes
+# 
+# # libraries ---------------------------------------------------------------
+# library(tidyverse)
+# library(DESeq2)
+# library(ComplexHeatmap)
+# 
+# # plot genes with heatmap -------------------------------------------------
+# vds_filter <- readRDS(file = "../../out/object/vds_all_filter.rds")
+# 
+# # gene_id <- rownames(assay(vds_filter)) %in% c("XIST","DDX3Y","RPS4Y1","USP9Y")
+# # gene_id <- rownames(assay(vds_filter)) %in% c("VCAN","CXCL12","ID2","ABI3BP","APLP1","MGP","TIMP3","SPOCK1","THY1","NID2","FBLN5","VEGFA")
+# # gene_id <- rownames(assay(vds_filter)) %in% c("SELP","C7","CPE")
+# gene_id <- rownames(assay(vds_filter)) %in% c("GJA5","SEMA3G","DKK2","FBLN5","CXCL12","ID2","ABI3BP","APLP1","MGP","SPOCK1","FBLN5","VEGFA","SELP","C7","CPE")
+# 
+# # notice that for both dataset the design didn't affect mucht he normalizationo,therefore the topmost variable genes are the same for both
+# # I will prduce the plot for just one dataset
+# # The heatmap becomes more interesting if we do not look at absolute expression strength but rather at the amount by which each gene deviates in a specific sample from the gene’s average across all samples. Hence, we center each genes’ values across samples, and plot a heatmap (figure below). We provide a data.frame that instructs the pheatmap function how to label the columns.
+# # mat <- assay(vds_filter)[topVarGenes, ]
+# mat <- assay(vds_filter)[gene_id, ]
+# mat2 <- (mat - rowMeans(mat))/rowSds(mat)
+# 
+# # change the rownames to match the symbol 
+# # rownames(mat2) <- rownames(mat2) %>% 
+# #   data.frame(ensembl=.) %>% 
+# #   left_join(DEG_1,by = "ensembl") %>% 
+# #   pull(symbol) 
+# 
+# # build the annotation object  
+# #
+# sample_ordered <- data.frame(sample = colnames(mat2)) %>%
+#   left_join(vds_filter@colData %>%
+#               data.frame(),by="sample")
+# 
+# # update the column name of the matrix
+# colnames(mat2) <- sample_ordered$sample_name
+# 
+# column_ha <- HeatmapAnnotation(treat = sample_ordered$treat,
+#                                gender = sample_ordered$gender,
+#                                col = list(treat = c("mock" = "gray", "BMP9" = "black"),
+#                                           gender = c("M" = "blue", "F" = "pink")))
+# 
+# ht2 <- Heatmap(mat2, 
+#                name = "exp",
+#                top_annotation = column_ha, 
+#                # cluster_rows = F, 
+#                # col = colorRamp2(c(-2, 0, 1), c("green", "white", "red")),
+#                # right_annotation = row_ha, 
+#                # row_split = rep(c(1,2,3,4),c(2,3,4,7)),
+#                column_title = "GOI") 
+# 
+# pdf("../../out/plot/heatmap_GOI_01.pdf",width = 6,height = 6) 
+# draw(ht2,heatmap_legend_side = "left",annotation_legend_side = "left") 
+# dev.off()
+# 
+# # plot genes dotplot ------------------------------------------------------
+# data <- readRDS("../../out/object/dds_all_filter_DESeq.rds")
+# 
+# lut <- data@colData %>%
+#   data.frame()
+# 
+# # GOI <- c("SMAD1","SMAD9","SMAD6","BMPR1B","SMAD7")
+# # GOI <- subset_genes
+# # GOI <- c("VCAN","CXCL12","ID2","ABI3BP","APLP1","MGP","TIMP3","SPOCK1","THY1","NID2","FBLN5","VEGFA")
+# # GOI <- c("SELP","C7","CPE")
+# # GOI <- c("GJA5","SEMA3G","DKK2","FBLN5")
+# GOI <- c("GJA5","SEMA3G","DKK2","FBLN5","CXCL12","ID2","ABI3BP","APLP1","MGP","SPOCK1","FBLN5","VEGFA","SELP","C7","CPE")
+# 
+# MR <- counts(data,normalized=T)%>%
+#   data.frame()%>%
+#   rownames_to_column("symbol") %>%
+#   pivot_longer(names_to = "sample",values_to = "exp",-symbol)%>%
+#   group_by(sample)%>%
+#   summarise(MR = sum(exp)/10^6)
+# 
+# # plot the data following the methods implemented in the plotCounts funciton from DESeq2
+# # Normalized counts plus a pseudocount of 0.5 are shown by default.
+# counts(data,normalized=T)%>%
+#   data.frame()%>%
+#   rownames_to_column("symbol") %>%
+#   dplyr::filter(symbol %in% GOI) %>%
+#   pivot_longer(names_to = "sample",values_to = "count",-symbol) %>%
+#   # add the milion reads per sample
+#   left_join(MR,by = "sample") %>%
+#   left_join(lut,by = "sample") %>%
+#   mutate(count_norm_adj = count + 0.5)%>%
+#   ggplot(aes(x=treat,y = count_norm_adj))+
+#   geom_boxplot(outlier.shape = NA)+
+#   geom_point(position = position_jitter(width = 0.1),alpha=0.6)+facet_wrap(~symbol,scales = "free")+scale_y_log10()+ theme_bw()+
+#   theme(strip.background = element_blank(), 
+#         panel.border = element_rect(colour = "black", fill = NA))
+# ggsave("../../out/plot/boxplot_GOI_01.pdf",width = 8,height = 8) 
+# 
+# counts(data,normalized=T)%>%
+#   data.frame()%>%
+#   rownames_to_column("symbol") %>%
+#   dplyr::filter(symbol %in% GOI) %>%
+#   pivot_longer(names_to = "sample",values_to = "count",-symbol) %>%
+#   # add the milion reads per sample
+#   left_join(MR,by = "sample") %>%
+#   left_join(lut,by = "sample") %>%
+#   mutate(count_norm_adj = count + 0.5)%>%
+#   ggplot(aes(x=treat,y = count_norm_adj,group=clone,col=clone))+geom_point(alpha=0.6)+
+#   geom_line() +
+#   facet_wrap(~symbol,scales = "free")+scale_y_log10()+ theme_bw()+
+#   theme(strip.background = element_blank(), 
+#         panel.border = element_rect(colour = "black", fill = NA))
+# ggsave("../../out/plot/scatterplot_GOI_01.pdf",width = 8,height = 8)
